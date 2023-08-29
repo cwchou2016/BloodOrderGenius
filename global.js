d44a9fe592bbb50c2328f9a78a188dbdc5b65d9e
cwchou2016 <s955401@gmail.com>
2023-08-29 20:02:26 +0800

    make status panel click through hide when panel is clicked

--- global.js
+++ global.js
@@ -5,4 +5,14 @@ function buildPluginStatus() {
   statusDiv.setAttribute("id", "plugin_status");
   statusDiv.innerHTML = "<label>血庫小精靈工作中....</label>";
   document.body.appendChild(statusDiv);
+
+  statusDiv.addEventListener("click", () => hideStatus());
+}
+
+function hideStatus() {
+  const statusDiv = document.getElementById("plugin_status");
+  statusDiv.className = "hide";
+  setTimeout(() => {
+    statusDiv.className = "";
+  }, 1000);
 }
