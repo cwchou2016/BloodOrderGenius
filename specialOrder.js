dae21c9d810d584240c3a976af251fd5003a360b
cwchou2016 <s955401@gmail.com>
2023-08-31 13:00:14 +0800

    create patient data obj and save to local storage

--- specialOrder.js
+++ specialOrder.js
@@ -15,6 +15,25 @@ function getPatient() {
   return document.getElementById("txt_patientNum").value;
 }
 
+async function savePatientData(ptid, data) {
+  chrome.storage.local.set({ [ptid]: data });
+
+  let res = await chrome.storage.local.get([ptid]);
+  console.log(res);
+}
+
+// Data process
+function createPatient(ptid) {
+  let pt = {
+    id: ptid,
+    rbc: [],
+    hla: [],
+    lastUpdate: Date.now(),
+  };
+
+  return pt;
+}
+
 // Modify UI
 function insertClickEvents() {
   document.getElementById("cbg1_0").addEventListener("click", () => {});
