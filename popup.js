97615a6fee8749e95a29785beae887528ebc9a5d
cwchou2016 <s955401@gmail.com>
2023-09-03 23:14:38 +0800

    save and load activation status in popup,html

--- popup.js
+++ popup.js
@@ -1,8 +1,39 @@
+window.addEventListener("load", () => {
+  initExtension();
+});
+
 document.getElementById("isActivate").addEventListener("click", () => {
   let check = document.getElementById("isActivate").checked;
   if (check) {
-    document.getElementById("status").innerText = "血庫小精靈工作中......";
+    turnOn();
   } else {
-    document.getElementById("status").innerText = "休息中.....";
+    turnOff();
   }
 });
+
+async function initExtension() {
+  let activate = await getExtensionActivation();
+  console.log(activate);
+  if (activate === false) {
+    turnOff();
+    return;
+  }
+  turnOn();
+}
+
+async function getExtensionActivation() {
+  let data = await chrome.storage.sync.get(["activate"]);
+  return data["activate"];
+}
+
+function turnOn() {
+  chrome.storage.sync.set({ ["activate"]: true });
+  document.getElementById("status").innerText = "血庫小精靈工作中......";
+  document.getElementById("isActivate").checked = true;
+}
+
+function turnOff() {
+  chrome.storage.sync.set({ ["activate"]: false });
+  document.getElementById("status").innerText = "休息中.....";
+  document.getElementById("isActivate").checked = false;
+}
