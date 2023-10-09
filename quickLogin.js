251fdbe595827f1491f9b06d9209703f87cc806d
cwchou2016 <s955401@gmail.com>
2023-10-09 21:58:31 +0800

    save hospital id when login btn is clicked

--- quickLogin.js
+++ quickLogin.js
@@ -2,4 +2,17 @@ window.addEventListener("load", async () => {
   if (await isExtensionOff()) return;
 
   buildPluginStatus();
+  insertClickEvents();
 });
+
+function saveHospitalId() {
+  let id = document.getElementById("txt_orgCode").value;
+  chrome.storage.sync.set({ ["hosp_id"]: id });
+}
+
+// Modify UI
+function insertClickEvents() {
+  document.getElementById("btn_send").addEventListener("click", (event) => {
+    saveHospitalId();
+  });
+}
