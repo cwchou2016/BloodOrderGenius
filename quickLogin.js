aed70eeef52ca6007d1ce070e43e72699d6a94f4
cwchou2016 <s955401@gmail.com>
2023-10-11 19:24:01 +0800

    fix: undefine hospital ID

--- quickLogin.js
+++ quickLogin.js
@@ -16,14 +16,17 @@ function autoFillValCode() {
 }
 
 async function autoFillHosptialId() {
-  let id = await chrome.storage.sync.get(["hosp_id"]);
+  let id = await chrome.storage.local.get(["hosp_id"]);
   id = id["hosp_id"];
+
+  if (id == null || id == "") return;
+
   document.getElementById("txt_orgCode").value = id;
 }
 
 function saveHospitalId() {
   let id = document.getElementById("txt_orgCode").value;
-  chrome.storage.sync.set({ ["hosp_id"]: id });
+  chrome.storage.local.set({ ["hosp_id"]: id });
 }
 
 // Modify UI
