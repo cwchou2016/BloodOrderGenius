4d8e28137e55cdf6703e0bad7e483c34d300e38e
cwchou2016 <s955401@gmail.com>
2026-08-01 14:34:40 +0800

    modify display text of plugin status

--- scripts/global.js
+++ scripts/global.js
@@ -100,7 +100,7 @@ function getEdiLink(orderNumber) {
 function buildPluginStatus() {
   const statusDiv = document.createElement("div");
   statusDiv.setAttribute("id", "plugin_status");
-  statusDiv.innerHTML = "<label>血庫小精靈工作中....</label>";
+  statusDiv.innerHTML = "<label>☺ 血庫小精靈工作中....</label>";
   document.body.appendChild(statusDiv);
 
   statusDiv.addEventListener("click", () => hideStatus());
