defcdd7747059fc1e2332d39cc2c1d2ceceee54d
cwchou2016 <s955401@gmail.com>
2026-08-01 14:12:23 +0800

    insert inventory table to bd_registed_table page

--- scripts/bd_registed_table.js
+++ scripts/bd_registed_table.js
@@ -3,9 +3,15 @@ window.addEventListener("load", async () => {
   
     buildPluginStatus();
     insertQuickNotes();
+    insertInventoryTable();
 });
 
 function insertQuickNotes() {
   let textarea = document.getElementById('textarea_note');
   buildQuickNotes(textarea);
+}
+
+function insertInventoryTable() {
+  const node = document.getElementById('queryResultBox').parentNode;
+  node.parentNode.insertBefore(buildInventory(), node);
 }
