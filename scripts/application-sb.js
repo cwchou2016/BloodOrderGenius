e63bd3be42dfbe58ee284eca756784bdd960e65c
cwchou2016 <s955401@gmail.com>
2026-08-05 21:49:10 +0800

    refractory codes: create buildNoteDiv()

--- scripts/application-sb.js
+++ scripts/application-sb.js
@@ -7,33 +7,40 @@ window.addEventListener("load", async function() {
 
 
 function connectSubmitBtn() {
-    document.getElementById("submit").addEventListener("click", () => {
-        const resultBoxDiv = document.getElementById("queryResultBox");
-        
-        
-        // Create an observer to watch for DOM changes inside #queryResultBox
-        const observer = new MutationObserver((mutationsList, observer) => {
-            // DOM has updated! Now grab the order number elements:
-            const orderEle = resultBoxDiv.querySelectorAll("table tbody tr td:nth-child(2)");
-            orderEle.forEach((ele) => {
-                const orderDiv = document.createElement("div");
-                orderDiv.innerText = ele.innerText;
+    document.getElementById('submit').addEventListener("click", () => {
+        buildNoteDivs();
+    });
+}
 
-                const noteDiv = document.createElement("div");
-                noteDiv.className = "note";
-                noteDiv.innerText = "notes"
 
-                ele.innerHTML = "";
-                ele.appendChild(orderDiv);
-                ele.appendChild(noteDiv);
-            });          
+/**
+ *  Build note divs below the order number
+ */
+function buildNoteDivs() {
+    const resultBoxDiv = document.getElementById("queryResultBox");
+    
+    
+    // Create an observer to watch for DOM changes inside #queryResultBox
+    const observer = new MutationObserver((mutationsList, observer) => {
+        // DOM has updated! Now grab the order number elements:
+        const orderEle = resultBoxDiv.querySelectorAll("table tbody tr td:nth-child(2)");
+        orderEle.forEach((ele) => {
+            const orderDiv = document.createElement("div");
+            orderDiv.innerText = ele.innerText;
 
-            // Stop observing once we've processed the update
-            observer.disconnect();
-        });
+            const noteDiv = document.createElement("div");
+            noteDiv.className = "note";
+            noteDiv.innerText = "notes"
 
-        // Start watching for changes to the table content
-        observer.observe(resultBoxDiv, { childList: true, subtree: true });
+            ele.innerHTML = "";
+            ele.appendChild(orderDiv);
+            ele.appendChild(noteDiv);
+        });          
+
+        // Stop observing once we've processed the update
+        observer.disconnect();
     });
 
+    // Start watching for changes to the table content
+    observer.observe(resultBoxDiv, { childList: true, subtree: true });
 }
