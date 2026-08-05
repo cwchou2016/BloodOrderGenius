d82f4fe8e903f6e57b65e9566f34f2582d9de291
cwchou2016 <s955401@gmail.com>
2026-08-05 23:26:57 +0800

    load antigen profile to the note div

--- scripts/application-sb.js
+++ scripts/application-sb.js
@@ -1,3 +1,5 @@
+let spDetail = null;
+
 window.addEventListener("load", async function() {
     if(await isExtensionOff()) return;
 
@@ -25,11 +27,12 @@ function buildNoteDivs() {
         const orderEle = resultBoxDiv.querySelectorAll("table tbody tr td:nth-child(2)");
         orderEle.forEach((ele) => {
             const orderDiv = document.createElement("div");
+            orderDiv.className = "orderNum"
             orderDiv.innerText = ele.innerText;
 
             const noteDiv = document.createElement("div");
             noteDiv.className = "note";
-            noteDiv.innerText = "notes"
+            noteDiv.innerText = "...";
 
             ele.innerHTML = "";
             ele.appendChild(orderDiv);
@@ -46,10 +49,43 @@ function buildNoteDivs() {
             });
         });
 
+        loadOrderNote();
+
         // Stop observing once we've processed the update
         observer.disconnect();
     });
 
     // Start watching for changes to the table content
     observer.observe(resultBoxDiv, { childList: true, subtree: true });
+}
+
+
+/**
+ * Load notes to note divs
+ */
+async function loadOrderNote() {
+    if (!spDetail) {
+        console.log("loading spDetail");
+        spDetail = await querySpBloodOrder();
+        console.log(spDetail);
+    }
+
+    const orderEle = document.querySelectorAll("#queryResultBox table tbody tr td:nth-child(2)");
+    orderEle.forEach((ele) => {
+        const orderNum = ele.getElementsByClassName("orderNum")[0].innerText;
+        const noteDiv = ele.getElementsByClassName("note")[0];
+        console.log(orderNum)
+        const result = spDetail.results.filter(order => order.spBldOrdNo === orderNum)[0];
+
+        // rbcAgneg
+        const rbcAg = [];
+        for(i=1; i<11;i++) {
+            const ag = result["rbcAgneg"+i];
+            if (ag) {
+                rbcAg.push(ag);
+            }
+        };
+
+        noteDiv.innerText = rbcAg.join(",")
+    });
 }
