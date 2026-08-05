fe79050291b8f65abd55e71da8e2c63f6f5a2dfc
cwchou2016 <s955401@gmail.com>
2026-08-05 20:59:38 +0800

    create connectSubmitBtn()

--- scripts/application-sb.js
+++ scripts/application-sb.js
@@ -2,4 +2,20 @@ window.addEventListener("load", async function() {
     if(await isExtensionOff()) return;
 
     buildPluginStatus();
+    connectSubmitBtn();
 });
+
+
+function connectSubmitBtn() {
+    document.getElementById("submit").addEventListener("click", () => {
+        console.log("clicked");
+        
+        const parentDiv = document.getElementById("queryResultBox");
+        const orderEle = parentDiv.querySelectorAll("table tbody tr td:nth-child(2)");
+
+        orderEle.forEach((ele) => {
+            console.log(ele);
+        });
+    });
+
+}
