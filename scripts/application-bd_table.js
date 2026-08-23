65af1c57a5c4bcdc8c783fb4bf227f9396d45228
cwchou2016 <s955401@gmail.com>
2026-08-23 18:56:02 +0800

    build note div beneath order number

--- scripts/application-bd_table.js
+++ scripts/application-bd_table.js
@@ -2,5 +2,57 @@ window.addEventListener("load", async function() {
     if(await isExtensionOff()) return;
     buildPluginStatus();
 
-
+    // connect submit button
+    document.getElementById('submit').addEventListener("click", () => {
+        buildNoteDivs();
+    });
+    
 });
+
+
+/**
+ *  Build note divs below the order number
+ */
+function buildNoteDivs() {
+    const resultBoxDiv = document.getElementById("queryResultBox");
+        
+    // Create an observer to watch for DOM changes inside #queryResultBox
+    const observer = new MutationObserver((mutationsList, observer) => {
+        // DOM has updated! Now grab the order number elements:
+        const orderEle = resultBoxDiv.querySelectorAll("table tbody tr td:nth-child(2)");
+        orderEle.forEach((ele) => {
+            const orderDiv = document.createElement("div");
+            orderDiv.className = "orderNum"
+            orderDiv.innerText = ele.innerText;
+
+            const noteDiv = document.createElement("div");
+            noteDiv.className = "note";
+
+            const productDiv = document.createElement("div");
+            productDiv.className = 'product';
+            productDiv.innerText="product details showing here";
+
+            noteDiv.appendChild(productDiv);
+
+            ele.innerHTML = "";
+            ele.appendChild(orderDiv);
+            ele.appendChild(noteDiv);
+        });
+
+        // add event to pager div
+        const pagerDiv = document.getElementsByClassName("pager")[0];
+        const pagerBtn = pagerDiv.querySelectorAll("li");
+
+        pagerBtn.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                buildNoteDivs();
+            });
+        });
+
+        // Stop observing once we've processed the update
+        observer.disconnect();
+    });
+
+    // Start watching for changes to the table content
+    observer.observe(resultBoxDiv, { childList: true, subtree: true });
+}
